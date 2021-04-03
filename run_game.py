c0f0d2847e3e74cba0c65cba811349fc45cde054
discretegames <discretizedgames@gmail.com>
2021-04-02 22:59:35 -0700

    Version checking

--- run_game.py
+++ run_game.py
@@ -1,5 +1,10 @@
 import code.game
+import sys
+
 if __name__ == "__main__":
+	min_version = 3, 9
+	if sys.version_info[:2] < min_version:
+		print("Python Prison expects Python {}.{} and may not work!".format(*min_version))
 	print("Started game")
 	code.game.run_game()
 	print("Ended game")
