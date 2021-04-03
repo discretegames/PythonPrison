241f99d646069d3f400e6f9e66a68f287b0eb466
discretegames <discretizedgames@gmail.com>
2021-04-02 23:14:51 -0700

    Tried pyinstller but not worth it.

--- run_game.py
+++ run_game.py
@@ -1,6 +1,8 @@
 import code.game
 import sys
 
+# Not bothering with pyinstaller exe since it makes the imports all wonky.
+
 if __name__ == "__main__":
 	min_version = 3, 9
 	if sys.version_info[:2] < min_version:
